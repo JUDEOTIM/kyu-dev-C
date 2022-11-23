cda75c6aae88c7012d120253618725ba3e7d95af
JUDEOTIM <otimjudeoj@gmail.com>
2022-11-23 00:50:27 -0800

    welcome notice to the user

---
+++ distance-speed-velocity/welcome.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+string name;
+cout<<"what is your name?...."  ;
+getline(cin, name);
+cout<<"welcome to SCD UGANDA LIMITED...."  <<name  ;
+
+return 0;
+
+}
