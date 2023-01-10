225718d06fc1bcc92f503a26cfe68c5455038b04
JUDEOTIM <otimjudeoj@gmail.com>
2023-01-10 02:32:33 -0800

    a program that converts meters to feet

---
+++ code-accademy/meters-to-feet.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+double L , F ;
+
+cout << "the length (L) in meters\n" ;
+cin >>L;
+F = L * 3.280839895 ;
+cout << "therefor the lenth (L) in feet (F) is = " <<F <<endl ;
+
+return 0;
+
+}
--- hello.cpp
+++ hello.cpp
@@ -7,7 +7,7 @@ string your_name;
 
 cout << "what is your name? \n";
 cin >>your_name;
-cout << "good morning "<<your_name; ;
+cout << "good morning " <<your_name <<endl ;
 
 return 0;
 
