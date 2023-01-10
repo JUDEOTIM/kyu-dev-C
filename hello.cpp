b53f56e829f162152f93d50ebebf78d5ec7a06bb
JUDEOTIM <otimjudeoj@gmail.com>
2023-01-10 00:41:51 -0800

    Print out a "Greetings!" message

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+string your_name;
+
+cout << "what is your name? \n";
+cin >>your_name;
+cout << "good morning "<<your_name; ;
+
+return 0;
+
+}
