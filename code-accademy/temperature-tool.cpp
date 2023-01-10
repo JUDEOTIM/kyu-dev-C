d5c430aa5ce4c8ef2682740b8501c86dcfe7365b
JUDEOTIM <otimjudeoj@gmail.com>
2023-01-10 06:33:54 -0800

    a program that takes inputted temperature in Celsius and returns the same temperature in Fahrenheit.

---
+++ code-accademy/temperature-tool.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+double temp;
+double T;
+cout <<"Enter initial temperature\n" ;
+cin >> T ;
+cout <<"the temperature in °C is = " <<T <<endl ;
+temp = (T * 9/5) + 32 ;
+cout <<"the temperature in °F is = " <<temp <<endl ;
+
+return 0;
+
+}
