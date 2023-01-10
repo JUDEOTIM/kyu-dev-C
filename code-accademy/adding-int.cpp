db8b519d91a5b3fb9306c23940de4184e1d5debd
JUDEOTIM <otimjudeoj@gmail.com>
2023-01-10 01:41:21 -0800

    a program that adds two integers

---
+++ code-accademy/adding-int.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
+int g , k , t , s ; 
+
+cout <<"the value of g\n" ;
+cin >>g;
+cout <<"the value of k\n" ;
+cin >>k;
+cout <<"the value of t\n" ;
+cin >>t;
+s = g + k + t;
+cout <<"the total sum is " <<s <<endl ;
+
+return 0;
+
+}
