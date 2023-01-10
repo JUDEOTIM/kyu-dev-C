423afef391ded32233ad37e436b991ee876f12d8
JUDEOTIM <otimjudeoj@gmail.com>
2023-01-10 04:47:12 -0800

    kyu paper

---
+++ kyu-test/if-statement.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+//local variable declarstion:
+int a = 10 ;
+
+// check the boolean condition
+if( a < 20 )
+{
+//if condition is true the print the following
+cout << "a is less than 20:" <<endl;
+}
+cout <<"value of a is:" <<a <<endl;
+
+return 0;
+
+}
