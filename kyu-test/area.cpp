c43f8afbed20aeefd1645f65df0a1dd4dad2ea11
JUDEOTIM <otimjudeoj@gmail.com>
2023-01-11 00:52:46 -0800

    finding the area of a room

---
+++ kyu-test/area.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+int room_width , room_length , room_area ;
+cout <<"Enter the width of the room: "<<endl;
+cin >> room_width;
+cout <<"Enter the length of the room: "<<endl ;
+cin >> room_length ;
+room_area = room_width * room_length ;
+cout <<"the area of the room is " <<room_area << " meters\n\n" << endl ;
+
+return 0 ;
+
+}
