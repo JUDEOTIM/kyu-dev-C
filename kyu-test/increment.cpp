3e444405f5b9f15f66309bbac30230c67d1c61d2
JUDEOTIM <otimjudeoj@gmail.com>
2023-01-11 01:47:35 -0800

    incrementing and decrementing

---
+++ kyu-test/increment.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+int a=5 , b=3 , c ;
+a++ ;
+c = ++a + b-- ;
+
+cout <<" a = " <<a <<endl  <<" b = " <<b <<endl <<" c = " <<c <<endl ;
+
+return 0;
+
+}
